5bb30a87e1d32ed286dd48c8c1be3baf064e8301
Connor Jordan <56240039+ComputerConnor@users.noreply.github.com>
2023-11-28 20:13:32 -0600

    Update script.js

--- sidebar/other/english/script.js
+++ sidebar/other/english/script.js
@@ -1,3 +1,25 @@
+// Add this function to display entries in the entries-container
+function displayEntries(entries) {
+    const entriesContainer = document.getElementById('entriesContainer');
+    entriesContainer.innerHTML = '';
+
+    entries.forEach(entry => {
+        const entryBox = document.createElement('div');
+        entryBox.classList.add('entry-box');
+
+        const nameHeading = document.createElement('h2');
+        nameHeading.textContent = entry.name;
+
+        const emailParagraph = document.createElement('p');
+        emailParagraph.textContent = entry.email;
+
+        entryBox.appendChild(nameHeading);
+        entryBox.appendChild(emailParagraph);
+
+        entriesContainer.appendChild(entryBox);
+    });
+}
+
 function submitForm() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
@@ -16,8 +38,16 @@ function submitForm() {
     .then(data => {
         alert(data.message); // Display a success message
     })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred. Please try again later.');
-    });
+    // Fetch and display entries after submission
+    fetch('/view_entries')
+        .then(response => response.json())
+        .then(data => {
+            if (data.entries) {
+                displayEntries(data.entries);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching entries:', error);
+            alert('An error occurred while fetching entries. Please try again later.');
+        });
 }
